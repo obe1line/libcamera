06fcf9ca636a6b04015c16fb94d1c07dd7deaf45
csz <christopher.storah@leicabiosystems.com>
2024-12-03 12:44:04 +1100

    Fix for buffer size compile error

--- src/ipa/rpi/cam_helper/cam_helper_imx500.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_imx500.cpp
@@ -257,7 +257,7 @@ void CamHelperImx500::parseInferenceData(libcamera::Span<const uint8_t> buffer,
 				exported.height = inputTensorInfo.height;
 				exported.numChannels = inputTensorInfo.channels;
 				strncpy(exported.networkName, inputTensorInfo.networkName.c_str(),
-					sizeof(exported.networkName));
+					sizeof(exported.networkName) - 1);
 				exported.networkName[sizeof(exported.networkName) - 1] = '\0';
 				metadata.set("cnn.input_tensor_info", exported);
 				metadata.set("cnn.input_tensor", std::move(inputTensorInfo.data));
